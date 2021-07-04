b3bb244c36d9a569f08a777edc37667db2d2262a
Aswathy Ashok <aswathyashok4127@gmail.com>
2021-07-04 12:28:45 +0530

    first

---
+++ learning_templates/basic_app/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import include
+from django.conf.urls import url
+from basic_app import views
+
+#TEMPLATING TAGGING
+app_name='basic_app'
+
+
+urlpatterns=[
+    url('relative/',views.relative,name='relative'),
+    url('other/',views.other,name='other'),
+
+    
+]
